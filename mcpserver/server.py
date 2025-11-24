7bde96a3e1a65f6680f6f97b87bf3609071709c7
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2025-11-24 16:17:21 +0100

    server.py added

---
+++ mcpserver/server.py
@@ -0,0 +1,89 @@
+from typing import Any
+import httpx
+from mcp.server.fastmcp import FastMCP
+
+# # Initialize FastMCP server
+# mcp = FastMCP("weather")
+
+
+# Create an MCP server
+mcp = FastMCP(
+    name = "weather",
+    host = "0.0.0.0", # only used for SEE transport (localhost)
+    port = 8080
+)
+
+# Contants
+NWS_API_BASE = "https://api.weather.gov"
+USER_AGENT = "weather-app/1.0"
+
+async def make_nws_request(url:str) -> dict[str, Any] | None:
+    """Make a request to the NWS API with proper error handling."""
+    headers = {
+        "User-Agent": USER_AGENT,
+        "Accept": "application/geo+json"
+    }
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(url, headers=headers, timeout=30.0)
+            response.raise_for_status()
+            return response.json()
+        except Exception:
+            return None
+        
+def format_alert(feature: dict) -> str:
+    """Format an alert feature into a readable string."""
+    props = feature["properties"]
+    return f"""
+        Event: {props.get("event", "Unknown")}
+        Area: {props.get("areDesc", "Unknown")}
+        Severity: {props.get("severity", "Unknown")}
+        Description: {props.get("description", "No description available")}
+        Instructions: {props.get("instruction", "No specific instructions provide")}
+        """
+
+
+@mcp.tool()
+async def get_alerts(state: str) -> str:
+    """Get weather alerts for a US State.
+
+    Args:
+        state: Two-letter NG State code (e.g. CA, NY, TX)
+    """
+    url = f"{NWS_API_BASE}/alerts/active?area={state}"
+    data = await make_nws_request(url)
+
+    if not data or "features" not in data:
+        return "Unable to fetch alerts or no alerts found."
+    
+    if not data["features"]:
+        return "No activate alerts for this state"
+    
+    alerts = [format_alert(feature) for feature in data["features"]]
+    return "\n---n".join(alerts)
+
+
+@mcp.resource("echo://{message}")
+def echo_resource(message: str) -> str:
+    """Read a message by message."""
+    # This would normally read from disk
+    return f"Resource echo: {message}"
+
+
+@mcp.resource("config://app")
+def get_config() -> str:
+    """Static configuration data."""
+    return "App configuration here"
+
+
+# Run the server
+if __name__ == "__main__":
+    transport == "sse"
+    if transport == "stdio":
+        print ("Running server with stdio transport")
+        mcp.run(transport="stdio")
+    elif transport == "sse":
+        print("Running server with SSE transport")
+        mcp.run(transport = "sse")
+    else:
+        raise ValueError(f"Unknown transport: {transport}")
