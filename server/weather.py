4984e4ecddcc8b085943360f983fc1a5dc443507
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2025-11-24 14:38:03 +0100

    mcp server

--- server/weather.py
+++ server/weather.py
@@ -6,5 +6,51 @@ from mcp.server.fastmcp import FastMCP
 mcp = FastMCP("weather")
 
 # Contants
-NWS_API_BASE = ""
+NWS_API_BASE = "https://api.weather.gov"
 USER_AGENT = "weather-app/1.0"
+
+async def make_nws_request(url:str) -> dict[str, Any] | None:
+    """Make a request to the NWS API with proper error handling."""
+    headers = {
+        "User-Agent": USER_AGENT,
+        "Accept": "application/geo+json"
+    }
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(url, headers=headers, timeout=30.0)
+            response.raise_for_status()
+            return response.json()
+        except Exception:
+            return None
+        
+def format_alert(feature: dict) -> str:
+    """Format an alert feature into a readable string."""
+    props = feature["properties"]
+    return f"""
+        Event: {props.get("event", "Unknown")}
+        Area: {props.get("areDesc", "Unknown")}
+        Severity: {props.get("severity", "Unknown")}
+        Description: {props.get("description", "No description available")}
+        Instructions: {props.get("instruction", "No specific instructions provide")}
+        """
+
+
+@mcp.tool()
+async def get_alerts(state: str) -> str:
+    """Get weather alerts for a US State.
+
+    Args:
+        state: Two-letter NG State code (e.g. CA, NY, TX)
+    """
+    url = f"{NWS_API_BASE}/alerts/active?area={state}"
+    data = await make_nws_request(url)
+
+    if not data or "features" not in data:
+        return "Unable to fetch alerts or no alerts found."
+    
+    if not data["features"]:
+        return "No activate alerts for this state"
+    
+    alerts = [format_alert(feature) for feature in data["features"]]
+    return "\n---n".join(alerts)
+
